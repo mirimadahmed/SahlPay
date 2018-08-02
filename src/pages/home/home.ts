cf8f7ab9d630fa75dd0d9f80ddbd9309435087ba
Waleed Arshad <waleedmalik677@gmail.com>
2018-08-02 07:48:34 +0500

    home.ts updated

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,13 +1,69 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
+import { NavParams,NavController,LoadingController, AlertController } from 'ionic-angular';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/timeout';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  apiUrl;
+  load;
+  success;
+  error;
+  amount;
+  username;
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams,private alert:AlertController,private loading:LoadingController,private http:Http) {
 
-  constructor(public navCtrl: NavController) {
+  }
+
+  ionViewDidLoad(){
+    this.getUserData();
+  }
+
+  getUserData(){
+    this.load = this.loading.create({
+      spinner:'crescent',
+      content:'Loading....'
+    });
+    this.load.present();
+
+    this.apiUrl = "http://10.0.38.117:3000/api/user/5b61cc5dd896513545970c93";
+      console.log(this.apiUrl);
+      this.http.get(this.apiUrl)
+      .timeout(6000)
+      .map( res => res.json() )
+      .subscribe( data => {
+
+        console.log(data);
+        if(data.error==undefined){  
+
+            if(data.status){
+              this.username = data.username;
+              this.amount = data.amount;
+              window.localStorage.setItem('user_id',data._id);
+              console.log(data._id);
+                 
+            }else{
+              this.error="Invalid Email or Password! ";
+              //this.Error=true;
+            
+            }
+        }
+    this.load.dismiss();
+      },
+      error =>{
+          console.log(error);
+          this.load.dismiss();
+          let alert = this.alert.create({
+            title: 'Oops!',
+            subTitle: 'There’s no network connection. Make sure you’re connected to a Wi-fi or mobile network and try again.',
+            buttons: ['Okay']
+          });
+          alert.present();
+      } );
 
   }
 
